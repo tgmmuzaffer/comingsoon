06898cb90bc3aa796b8b0bce259e6a60361536da
muzaffer <muzaffer.deveci@tazebt.com>
2022-01-07 17:33:18 +0300

    aler and mail config end

--- comingsoon/comingsoon/Controllers/HomeController.cs
+++ comingsoon/comingsoon/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using comingsoon.Models;
+using MailKit.Net.Smtp;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MimeKit;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -23,11 +25,57 @@ namespace comingsoon.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
+        [HttpPost]
+        public IActionResult Mailsender(Mail mail)
+
+        {
+            try
+            {
+                MimeMessage m = new MimeMessage();
+                BodyBuilder bodyBuilder = new BodyBuilder();
+                SmtpClient smtp = new SmtpClient();
+                bodyBuilder.HtmlBody = "Talebinizi aldık.En kısa sürede dönüş yapılacaktır.";
+                MailboxAddress from = new MailboxAddress("Camaşırhane Ekipmanları", "info@camasirhaneekipmanlari.com");
+                MailboxAddress to = new MailboxAddress(mail.emailAddress, mail.emailAddress);
+                m.From.Add(from);
+                m.Body = bodyBuilder.ToMessageBody();
+                m.Subject = "Bilgilendirme";
+                m.To.Add(to);
+                smtp.Connect("win5.wlsrv.com", 465, true);
+                smtp.Authenticate("ugur.yalcin@camasirhaneekipmanlari.com", "$q1q9T3u");
+                smtp.Send(m);
+                smtp.Disconnect(true);
+                smtp.Dispose();
+
+
+                MimeMessage mG = new MimeMessage();
+                BodyBuilder bodyBuilderG = new BodyBuilder();
+                SmtpClient smtpG = new SmtpClient();
+                bodyBuilderG.HtmlBody = string.Format(@"<p>Kimden: <a href='mailto: {0}'>{1}</a></p><br><p>Mesaj İçeriği: {2}</p>", mail.emailAddress,mail.emailAddress, mail.message);
+                MailboxAddress fromG = new MailboxAddress("ComingSoon Site", "info@camasirhaneekipmanlari.com");
+                MailboxAddress toG = new MailboxAddress(mail.emailAddress, "ugur.yalcin@camasirhaneekipmanlari.com");
+                mG.From.Add(fromG);
+                mG.Body = bodyBuilderG.ToMessageBody();
+                mG.Subject = "Bilgilendirme";
+                mG.To.Add(toG);
+                smtpG.Connect("win5.wlsrv.com", 465, true);
+                smtpG.Authenticate("info@camasirhaneekipmanlari.com", "Yamahar123+-*/");
+                smtpG.Send(mG);
+                smtpG.Disconnect(true);
+                smtpG.Dispose();
+
+                TempData["success"] = "Mesajınız gönderilmiştir.";
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception e)
+            {
+                TempData["fail"] = "Mesajınız gönderilemedi.";
+                return RedirectToAction("Index");
+            }
+
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ comingsoon/comingsoon/Models/Mail.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace comingsoon.Models
+{
+    public class Mail
+    {
+        [Required(ErrorMessage = "Lütfen Doldurunuz")]
+        [EmailAddress(ErrorMessage = "Lütfen Geçerli Bir E-Posta Adresi Giriniz")]
+        public string emailAddress { get; set; }
+        [Required(ErrorMessage = "Lütfen Doldurunuz")]
+        [MinLength(3, ErrorMessage = "3 Karakterden Fazla Olmalı")]
+        [MaxLength(200, ErrorMessage = "200 Karakterden Fazla Olmamalı")]
+        public string message { get; set; }
+    }
+}
